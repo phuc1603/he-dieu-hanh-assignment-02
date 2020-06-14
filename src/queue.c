82eb8f62127d26cf1750ded9204c077f87f328c3
phuc1603 <54540308+phuc1603@users.noreply.github.com>
2020-06-14 14:02:09 +0700

    Update queue.c

--- src/queue.c
+++ src/queue.c
@@ -10,10 +10,25 @@ void enqueue(struct queue_t * q, struct pcb_t * proc) {
 	/* TODO: put a new process to queue [q] */	
 }
 
-struct pcb_t * dequeue(struct queue_t * q) {
-	/* TODO: return a pcb whose prioprity is the highest
-	 * in the queue [q] and remember to remove it from q
-	 * */
-	return NULL;
+struct pcb_t* dequeue(struct queue_t *q) {
+    struct pcb_t* proc = NULL;
+    if (empty(q)) {
+        proc = NULL;
+    }
+    else {
+        int max = 0;
+        uint32_t priorMax = q->proc[0]->priority;
+        for (int i = 0; i < q->size; i++) {
+            if (q->proc[i]->priority > priorMax) {
+                priorMax = q->proc[i]->priority;
+                max = i;
+            }
+        }
+        proc = q->proc[max];
+        for (int i = max; i < q->size - 1; i++) {
+            q->proc[i] = q->proc[i + 1];
+        }
+        q->size--;
+    }
+    return proc;
 }
-
